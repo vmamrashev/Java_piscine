b55e6d837c30dfa3369cf603c0a02e22c721636b
Super Thaddeus <supertha@student.21-school.ru>
2024-07-24 00:16:22 +0700

    day00 ex01 - done

--- day00/src/ex01/Program.java
+++ day00/src/ex01/Program.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
+        int input = 0;
+        if (sc.hasNextInt()){
+            input = sc.nextInt();
+        }
         System.out.println(isPrime(input));
     }
 
     static boolean isPrime(int n){
-        if (n%2 == 0) n = 2;
+        if (n%2 == 0 || n == 1) {
+            return n == 2;
+        }
+
         int d = 3;
         while (d * d <= n && n % d !=0) {
             d += 2;
